ba67f342b5f0399c41cfe5edfcbafa4b3653df08
surendra15695 <104766418+surendra15695@users.noreply.github.com>
2025-05-12 10:02:02 +0530

    Update environment.prod.ts

--- Application.WebApp/ClientApp/src/environments/environment.prod.ts
+++ Application.WebApp/ClientApp/src/environments/environment.prod.ts
@@ -3,17 +3,17 @@ export const environment = {
   production: true,
 
   //For UAT AppGateway
-  // apiurl: 'https://mrfconnectuat.mrfindia.net/api',
-  // apppath: 'https://mrfconnectuat.mrfindia.net:5001',
-  // apiKey: "embee.co.in",
+   apiurl: 'https://mrfconnectuat.mrfindia.net/api',
+   apppath: 'https://mrfconnectuat.mrfindia.net:5001',
+   apiKey: "embee.co.in",
   // UAT Email Link
-  //emailLink: "https://mrfconnectuat.mrfindia.net/"
+  emailLink: "https://mrfconnectuat.mrfindia.net/"
 
   //For Production AppGateway
-  apiurl: 'https://mrfconnect.mrfindia.net/api',
-  apppath: 'https://mrfconnect.mrfindia.net:5001',
-  apiKey: "embee.co.in",
+  //apiurl: 'https://mrfconnect.mrfindia.net/api',
+  //apppath: 'https://mrfconnect.mrfindia.net:5001',
+  //apiKey: "embee.co.in",
   // PROD Email Link
-  emailLink: "https://mrfconnect.mrfindia.net/"
+  //emailLink: "https://mrfconnect.mrfindia.net/"
 
 };
